01c4a84adf40b30a648b998b609e1950a6259976
Daniel Wrafter <dwrafter@mit.edu>
2019-12-07 14:46:52 -0500

    Circular mean averaging

--- model.py
+++ model.py
@@ -4,14 +4,26 @@ alpha = 20*2*np.pi/360  #20 deg in radians
 theta = np.random.uniform(-alpha, alpha)
 
 
+def angle_mean(thetas):
+    """
+    Input: array of angles
+    Output: circular mean of the angles
+
+    https://en.wikipedia.org/wiki/Mean_of_circular_quantities
+    """
+    x = np.mean(np.cos(thetas))
+    y = np.mean(np.sin(thetas))
+    theta_mean = np.atan2(y, x)
+    return theta_mean
+
+
 def make_s(num_points, motion_coherence):
-    motions = np.zeros((num_points, 1))
+    motions = np.random.uniform(low=-np.pi, high=np.pi, size = num_points)
+    # set N/MC of the particles to the same value
     num_coherent = int(num_points * motion_coherence)
-    s = num_coherent * theta
-    for i in range(num_points - num_coherent):
-        rand_angle = np.random.uniform(-np.pi, np.pi)
-        s += rand_angle
-    return s/num_points
+    motions[:num_points] = theta
+    return angle_mean(motions)
+    
 
 def add_noise(s, num_points, motion_coherence, mean = 0):
     var = 1/motion_coherence/100
