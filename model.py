661ca944f0ffdc4c6bc9b2e7a0526dc383767992
Crystal Wang <cwang506@mit.edu>
2019-12-07 15:28:11 -0500

    pipeline

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import probability_utils
 
 
 alpha = 20*2*np.pi/360  #20 deg in radians
-theta = np.random.uniform(-alpha, alpha)
+theta = 0.01#np.random.uniform(-alpha, alpha)
 
 
 def angle_mean(thetas):
@@ -29,23 +29,44 @@ def make_s(num_points, motion_coherence):
 
 def add_noise(s, num_points, motion_coherence, mean = 0):
     var = 1/motion_coherence/100
-    noise = np.random.normal(mean, num_points**2*var)
+    noise = np.random.normal(mean, var)
+    print("noise: ", noise)
     return s + noise
 
+def normalize_angle(ang):
+    while ang < -np.pi:
+        ang += 2 * np.pi
+    while ang > np.pi:
+        ang -= 2*np.pi
+    return ang
+
 def main():
-    print(theta)
     MC = 0.03
     num_points = 100
     prior_on_left = 0.5
     s = make_s(num_points, MC)
-    m = add_noise(s, num_points, MC)
-    p_h_m_left = probability_utils.p_h_given_m('left', m, MC, prior_on_left)
-    p_h_m_right = probability_utils.p_h_given_m('right', m, MC, prior_on_left)
+    print("s: ", s)
+    m = normalize_angle(add_noise(s, num_points, MC))
+    print("m: ", m)
+    p_h_m_left = probability_utils.p_h_given_m('left', m, MC, prior_on_left, alpha)
+    print(p_h_m_left)
+    
+    p_h_m_right = probability_utils.p_h_given_m('right', m, MC, prior_on_left, alpha)
+    print(p_h_m_right)
     if p_h_m_left >= p_h_m_right:
         hmap = 'left'
     else:
         hmap = 'right'
     print(hmap)
 
+    max_theta = None
+    max_p_theta = 0
+    epsilon = 0.5
+    for theta in np.linspace(-alpha, alpha, 10):
+        p_theta = probability_utils.p_theta_given_m(m, hmap, theta, MC, alpha)
+        if p_theta > max_p_theta:
+            max_theta = theta
+            max_p_theta = p_theta
+    print(max_theta)
 if __name__ == "__main__":
     main()
--- probability_utils.py
+++ probability_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 # import matplotlib.pyplot as plt
 from scipy.stats import rv_continuous, uniform, norm
 from scipy.integrate import quad
-from model import alpha
 
 
 def p_m_given_theta_h(theta, hypothesis, MC):
@@ -19,7 +18,7 @@ def p_m_given_theta_h(theta, hypothesis, MC):
     return n
 
 
-def p_theta_given_h(h):
+def p_theta_given_h(h, alpha):
     if h == 'left':
         u = uniform(loc=-alpha, scale=  alpha)
     elif h == 'right':
@@ -28,9 +27,8 @@ def p_theta_given_h(h):
         print("h should be left or right")
     return u
 
-def p_m_given_h(m, h, MC):
-    total_sum = 0
-    func = lambda theta: p_theta_given_h(h).pdf(theta)*p_m_given_theta_h(theta, h, MC).pdf(m)
+def p_m_given_h(m, h, MC, alpha):
+    func = lambda theta: p_theta_given_h(h, alpha).pdf(theta)*p_m_given_theta_h(theta, h, MC).pdf(m)
     prob = quad(func, -np.pi, np.pi)
     return prob[0]
 
@@ -40,12 +38,16 @@ def p_h(h, prior_on_left):
     else:
         return 1 - prior_on_left
 
-def p_h_given_m(h, m, MC, prior_on_left):
-    return p_m_given_h(m, h, MC)*p_h(h, prior_on_left)
+def p_h_given_m(h, m, MC, prior_on_left, alpha):
+    num = p_m_given_h(m, h, MC, alpha)
+    return num*p_h(h, prior_on_left)
 
-def p_theta_given_m(m, hmap, theta, MC):
-    num = p_m_given_theta_h(theta, hmap, MC).pdf(m)*p_theta_given_h(hmap).pdf(theta)
-    denom = p_m_given_h(m, hmap, MC)
+def p_theta_given_m(m, hmap, theta, MC, alpha):
+    print(m, theta)
+    num = p_m_given_theta_h(theta, hmap, MC).pdf(m)*p_theta_given_h(hmap, alpha).pdf(theta)
+    denom = p_m_given_h(m, hmap, MC, alpha)
+    print(num)
+    print(denom)
     return num/denom
 
 
@@ -60,7 +62,8 @@ def p_theta_given_m(m, hmap, theta, MC):
 # ax.plot(x, u.pdf(x))
 # plt.show()
 
-# u = p_m_given_theta_h(0.2,'right', 0.01)
+# u = p_m_given_h(0.2,'right', 0.03, 0.359)
+# print(u)
 # x = np.linspace(u.ppf(0.01), u.ppf(0.99), 100)
 # fig, ax = plt.subplots(1, 1)
 # ax.plot(x, u.pdf(x))
