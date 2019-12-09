8c951e2a3df31df464f770301543de174f8525ae
Crystal Wang <cwang506@mit.edu>
2019-12-08 21:16:52 -0500

    changed max search over 70

--- model.py
+++ model.py
@@ -63,7 +63,7 @@ def main(theta, MC):
     max_theta = None
     max_p_theta = 0
     epsilon = 0.5
-    for th in np.linspace(-alpha, alpha, 10):
+    for th in np.linspace(-alpha , alpha, 100):#np.linspace(-alpha, alpha, 10):
         p_theta = probability_utils.p_theta_given_m(m, hmap, th, MC, alpha)
         if p_theta > max_p_theta:
             max_theta = th
@@ -72,61 +72,61 @@ def main(theta, MC):
 
 
 if __name__ == "__main__":
-    num_iterations = 40
+    num_iterations = 3
     num_datapoints_on_each_side = 7
     color_dic = {
         0.03: [0, 0, 1],
         0.06: [1, 0, 0],
         0.12: [0, 1, 0]
     }
-    # MC_thetas = {}
-    # MC_probs = {}
-    # for MC in [0.03, 0.06, 0.12]:
-    #     print("MC: ", MC)
-    #     thetas = []
-    #     max_thetas= []
-    #     probs = []
-    #     for theta in np.linspace(-alpha, alpha, 2*num_datapoints_on_each_side+1):
-    #         tot_angle = []
-    #         tot_probability = 0
-    #         for i in range(num_iterations):
-    #             max_theta, p_right = main(theta, MC)
-    #             tot_angle.append(max_theta)
-    #             tot_probability+=p_right
-    #         mean_angle = angle_mean(np.array(tot_angle))
-    #         max_thetas.append(mean_angle * 360 / (2*np.pi))
-    #         probs.append(tot_probability/num_iterations) 
-    #         thetas.append(theta * 360/(2*np.pi))
-    #         print("result: ", theta, mean_angle, tot_probability/num_iterations)
-    #     # ax.plot(thetas, max_thetas, color=color_dic[MC], label="Motion Coherence of: %s" %MC)
-    #     # ax.plot(thetas, probs, color=color_dic[MC], label="Motion Coherence of: %s"%MC)
-    #     MC_thetas[MC] = max_thetas
-    #     MC_probs[MC] = probs
-    #     print(thetas)
-    #     print(max_thetas)
-    #     print(probs)
-    # print(MC_thetas)
-    # print(MC_probs)
-    # print(thetas)
+    MC_thetas = {}
+    MC_probs = {}
+    for MC in [0.03, 0.06, 0.12]:
+        print("MC: ", MC)
+        thetas = []
+        max_thetas= []
+        probs = []
+        for theta in np.linspace(-alpha, alpha, 2*num_datapoints_on_each_side+1):
+            tot_angle = []
+            tot_probability = 0
+            for i in range(num_iterations):
+                max_theta, p_right = main(theta, MC)
+                tot_angle.append(max_theta)
+                tot_probability+=p_right
+            mean_angle = angle_mean(np.array(tot_angle))
+            max_thetas.append(mean_angle * 360 / (2*np.pi))
+            probs.append(tot_probability/num_iterations) 
+            thetas.append(theta * 360/(2*np.pi))
+            print("result: ", theta, mean_angle, tot_probability/num_iterations)
+        # ax.plot(thetas, max_thetas, color=color_dic[MC], label="Motion Coherence of: %s" %MC)
+        # ax.plot(thetas, probs, color=color_dic[MC], label="Motion Coherence of: %s"%MC)
+        MC_thetas[MC] = max_thetas
+        MC_probs[MC] = probs
+        print(thetas)
+        print(max_thetas)
+        print(probs)
+    print(MC_thetas)
+    print(MC_probs)
+    print(thetas)
     
-    max_theta_pickle_path = "pickled_data/max_thetas_%s_datapoints_%s_iterations" %(num_datapoints_on_each_side*2+1, num_iterations)
-    probs_pickle_path = "pickled_data/probs_to_right_%s_datapoints_%s_iterations" %(num_datapoints_on_each_side*2+1, num_iterations)
-    thetas_pickle_path = "pickled_data/thetas_linspace_%s_datapoints_%s_iterations" %(num_datapoints_on_each_side*2+1, num_iterations)
+    max_theta_pickle_path = "pickled_data/max_thetas_%s_datapoints_%s_iterations_v2" %(num_datapoints_on_each_side*2+1, num_iterations)
+    probs_pickle_path = "pickled_data/probs_to_right_%s_datapoints_%s_iterations_v2" %(num_datapoints_on_each_side*2+1, num_iterations)
+    thetas_pickle_path = "pickled_data/thetas_linspace_%s_datapoints_%s_iterations_v2" %(num_datapoints_on_each_side*2+1, num_iterations)
 
-    # with open(max_theta_pickle_path, 'wb') as f:
-    #     pickle.dump(MC_thetas, f)
-    # with open(probs_pickle_path, 'wb') as f:
-    #     pickle.dump(MC_probs, f)
-    # with open(thetas_pickle_path, 'wb') as f:
-    #     pickle.dump(thetas)
+    with open(max_theta_pickle_path, 'wb') as f:
+        pickle.dump(MC_thetas, f)
+    with open(probs_pickle_path, 'wb') as f:
+        pickle.dump(MC_probs, f)
+    with open(thetas_pickle_path, 'wb') as f:
+        pickle.dump(thetas,f)
 
 
-    with open(max_theta_pickle_path, 'rb') as f:
-        MC_thetas = pickle.load(f)
-    with open(probs_pickle_path, 'rb') as f:
-        MC_probs = pickle.load(f)
-    with open(thetas_pickle_path, 'rb') as f:
-        thetas = pickle.load(f)
+    # with open(max_theta_pickle_path, 'rb') as f:
+    #     MC_thetas = pickle.load(f)
+    # with open(probs_pickle_path, 'rb') as f:
+    #     MC_probs = pickle.load(f)
+    # with open(thetas_pickle_path, 'rb') as f:
+    #     thetas = pickle.load(f)
 
     #plot max thetas
     fig, ax = plt.subplots(1, 1)
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     plt.ylabel("Estimated Direction")
     plt.title("Estimated Direction vs Actual Degree")
     ax.legend()
-    plt.savefig("plots/motion_estimation_main_MC_%s_datapoints_%s_iterations.png" %(num_datapoints_on_each_side*2+1, num_iterations))
+    plt.savefig("plots/motion_estimation_main_MC_%s_datapoints_%s_iterations_v2.png" %(num_datapoints_on_each_side*2+1, num_iterations))
     plt.show()
 
     fig, ax = plt.subplots(1, 1)
@@ -145,5 +145,5 @@ if __name__ == "__main__":
     plt.xlabel("Actual Degree")
     plt.ylabel("Fraction motion right of reference")
     ax.legend()
-    plt.savefig("plots/fraction_to_the_right_MC_%s_datapoints_%s_iterations.png" %(num_datapoints_on_each_side*2+1, num_iterations))
+    plt.savefig("plots/fraction_to_the_right_MC_%s_datapoints_%s_iterations_v2.png" %(num_datapoints_on_each_side*2+1, num_iterations))
     plt.show()
